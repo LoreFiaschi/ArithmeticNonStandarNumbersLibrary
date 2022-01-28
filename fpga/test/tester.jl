596063b57296385c6e2c52d9e8b88311545fb18f
Lorenzo Fiaschi <35898216+LoreFiaschi@users.noreply.github.com>
2022-01-28 10:15:00 +0100

    Small improvement of tester.jl

--- fpga/test/tester.jl
+++ fpga/test/tester.jl
@@ -4,31 +4,48 @@ using .BAN
 open("data.txt", "r") do ff
 open("out_jl.txt", "w") do out
 
-    SAMPLES =  parse(Int32, readuntil(ff," ")) #read(ff, Int32);
-    SIZE = parse(Int32, readuntil(ff,"\n"))
-    SAMPLES = 1;
+    SAMPLES =  parse(Int32, readuntil(ff," ")); #read(ff, Int32);
+    SIZE = parse(Int32, readuntil(ff,"\n"));
+    SAMPLES = 2;
     for i=1:SAMPLES
-        p1 = parse(Int32, readuntil(ff," "))
-        num1 = Vector{Float32}(undef, SIZE)
+        p1 = parse(Int32, readuntil(ff," "));
+        num1 = Vector{Float32}(undef, SIZE);
         for j=1:SIZE
-            num1[j] = parse(Float32, readuntil(ff," "))
+            num1[j] = parse(Float32, readuntil(ff," "));
         end
         
-        b1 = Ban(p1, num1)
+        b1 = Ban(p1, num1);
 
         open("data.txt") do ff2
            
-            for ii=1:SAMPLES
-                p2 = parse(Int32, readuntil(ff," "))
-                num2 = Vector{Float32}(undef, SIZE)
+			for ii=1:(i+1)
+				readline(ff2);
+			end
+		   
+            for ii=(i+2):SAMPLES
+                p2 = parse(Int32, readuntil(ff," "));
+                num2 = Vector{Float32}(undef, SIZE);
                 for j=1:SIZE
-                    num2[j] = parse(Float32, readuntil(ff," "))
+                    num2[j] = parse(Float32, readuntil(ff," "));
                 end
                 
-                b2 = Ban(p2, num2)
+                b2 = Ban(p2, num2);
 
-                println(b1)
-                println(b2)
+                write(out, b1+b2);
+				write(out, b1-b2);
+				write(out, b1*b2);
+				write(out, b1/b2);
+				write(out, b2/b1);
+				write(out, b1==b2);
+				write(out, b1<b2);
+				write(out, b1^2);
+				write(out, b1^(-2));
+				write(out, b1^5);
+				write(out, b1^(-5));
+				write(out, abs(b1));
+				if b1>=0 && degree(b1)%2 == 0
+					write(out, sqrt(b1));
+				end
             end 
         end
     end
