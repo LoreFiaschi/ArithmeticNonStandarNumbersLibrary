81f4580fe96bc3df8f4b5a7fd60d5d6aa4f8816b
GSilvestri92 <56162573+GSilvestri92@users.noreply.github.com>
2022-02-03 01:19:25 +0100

    Updated version of BanPlots.jl
    The plot function now prints in one color (steelblue, close to standard color for first series) and scatterplots are added to plot single value intervals.
    Keyword argument rlplot added in order to plot rewards when dealing with non-Archimedean LMORL

--- src/BanPlots.jl
+++ src/BanPlots.jl
@@ -16,7 +16,12 @@ function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
     
     start_idx = 1;
     
-    figtitle, figsize, title = retrieve_args(kwargs);
+    figtitle, figsize, title, rlplot = retrieve_args(kwargs);
+
+    if rlplot
+        #We are interested in the objectives plot only
+        num_subplot_y = BAN.SIZE;
+    end
     
     fig = figure(figtitle,figsize=figsize)
     subplots_adjust(hspace=0.0)
@@ -40,17 +45,54 @@ function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
         end
 
         ref_deg_y = degy[start_idx];
-        intervals_y = [];
-        
+        intervals_y = [];  
+
         for j=start_idx:idx #since they are consecutive indexes
             degy[j] != ref_deg_y && (push!(intervals_y, (start_idx, j-1)); start_idx=j; ref_deg_y=degy[j]; true)
         end
         push!(intervals_y, (start_idx, idx));
 
-        for j in intervals_y
-            axs[max_degree_y-degy[j[1]]+1].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[1] for z in y[j[1]:j[2]]]);
+        if rlplot #if reinforcement learning reward plot flag is active
+            for j in intervals_y
+                #if first objective value is non-zero, simply plot each reward
+                if degy[j[1]]==max_degree_y
+                    for n=1:BAN.SIZE
+                        if j[1]==j[2]
+                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[n] for z in y[j[1]:j[2]]], s=10,c = :steelblue);
+                        else   
+                            axs[n].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[n] for z in y[j[1]:j[2]]], c=:steelblue);
+                        end
+                    end
+                #if first objective value is zero, plot zero(es) for the interval in all objectives prior the first degree of Ban's normal form
+                else
+                    for n=1:max_degree_y-degy[j[1]]
+                        if j[1]==j[2]
+                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], 0, s=10,c = :steelblue);
+                        else   
+                            axs[n].plot([z.num[1] for z in x[j[1]:j[2]]], zeros(j[2]-j[1]+1), c=:steelblue);
+                        end
+                    end
+                    ind=0
+                    for n=max_degree_y-degy[j[1]]+1:BAN.SIZE
+                        ind+=1
+                        if j[1]==j[2]
+                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[ind] for z in y[j[1]:j[2]]], s=10,c = :steelblue);
+                        else   
+                            axs[n].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[ind] for z in y[j[1]:j[2]]], c=:steelblue);
+                        end
+                    end
+                end
+            end
+        else
+            for j in intervals_y
+                if j[1]==j[2]
+                    axs[max_degree_y-degy[j[1]]+1].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[1] for z in y[j[1]:j[2]]], s=10,c = :steelblue);
+                else   
+                    axs[max_degree_y-degy[j[1]]+1].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[1] for z in y[j[1]:j[2]]], c=:steelblue);
+                end
+            end
         end
-        
+
         start_idx=idx+1;
     end
 end
@@ -60,6 +102,7 @@ function retrieve_args(args)
     figtitle = (haskey(args, :figtitle)) ? args[:figtitle] : "";
     figsize = (haskey(args, :figsize) ? args[:figsize] : (10,10));
     title = (haskey(args, :title) ? args[:title] : "");
-    
-    return figtitle, figsize, title
+    rlplot = (haskey(args, :rlplot) ? args[:rlplot] : false);
+
+    return figtitle, figsize, title, rlplot;
 end
