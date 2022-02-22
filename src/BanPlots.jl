4f7689b9b1caf5cdddca5eb5f79b9719f56c6f6e
GSilvestri92 <56162573+GSilvestri92@users.noreply.github.com>
2022-02-22 15:10:26 +0100

    Added Plot for Ban_isbits

--- src/BanPlots.jl
+++ src/BanPlots.jl
@@ -3,6 +3,16 @@ using PyPlot;
 
 function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
 
+    if isbits(y[1])
+        launch_plot_for_isbits(x,y,kwargs)
+    else
+        launch_plot_for_non_isbits(x,y,kwargs)
+    end
+end     
+#################################################################
+# Standard Ban Plot
+#################################################################
+function launch_plot_for_non_isbits(x::Vector{Ban}, y::Vector{Ban}, kwargs)
     degx = [degree(x[i]) for i in eachindex(x)];
     degy = [degree(y[i]) for i in eachindex(y)];
 
@@ -58,7 +68,7 @@ function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
                 if degy[j[1]]==max_degree_y
                     for n=1:BAN.SIZE
                         if j[1]==j[2]
-                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[n] for z in y[j[1]:j[2]]], s=10,c = :steelblue);
+                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[n] for z in y[j[1]:j[2]]], s=3,c = :steelblue);
                         else   
                             axs[n].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[n] for z in y[j[1]:j[2]]], c=:steelblue);
                         end
@@ -67,7 +77,7 @@ function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
                 else
                     for n=1:max_degree_y-degy[j[1]]
                         if j[1]==j[2]
-                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], 0, s=10,c = :steelblue);
+                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], 0, s=3,c = :steelblue);
                         else   
                             axs[n].plot([z.num[1] for z in x[j[1]:j[2]]], zeros(j[2]-j[1]+1), c=:steelblue);
                         end
@@ -76,7 +86,7 @@ function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
                     for n=max_degree_y-degy[j[1]]+1:BAN.SIZE
                         ind+=1
                         if j[1]==j[2]
-                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[ind] for z in y[j[1]:j[2]]], s=10,c = :steelblue);
+                            axs[n].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[ind] for z in y[j[1]:j[2]]], s=3,c = :steelblue);
                         else   
                             axs[n].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[ind] for z in y[j[1]:j[2]]], c=:steelblue);
                         end
@@ -86,9 +96,115 @@ function plot(x::Vector{Ban}, y::Vector{Ban}; kwargs...)
         else
             for j in intervals_y
                 if j[1]==j[2]
-                    axs[max_degree_y-degy[j[1]]+1].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[1] for z in y[j[1]:j[2]]], s=10,c = :steelblue);
+                    axs[max_degree_y-degy[j[1]]+1].scatter([z.num[1] for z in x[j[1]:j[2]]], [z.num[1] for z in y[j[1]:j[2]]], s=3,c = :steelblue);
                 else   
                     axs[max_degree_y-degy[j[1]]+1].plot([z.num[1] for z in x[j[1]:j[2]]], [z.num[1] for z in y[j[1]:j[2]]], c=:steelblue);
+                end
+            end
+        end
+
+        start_idx=idx+1;
+    end
+end
+
+#################################################################
+# Size 3 isbits Ban Plot
+#################################################################
+function launch_plot_for_isbits(x::Vector{Ban}, y::Vector{Ban}, kwargs)
+    degx = [degree(x[i]) for i in eachindex(x)];
+    degy = [degree(y[i]) for i in eachindex(y)];
+
+    max_degree_x = maximum(degx);
+    min_degree_x = minimum(degx);
+    max_degree_y = maximum(degy);
+    min_degree_y = minimum(degy);
+
+    num_subplot_x = max_degree_x-min_degree_x+1;
+    num_subplot_y = max_degree_y-min_degree_y+1;
+    
+    start_idx = 1;
+    
+    figtitle, figsize, title, rlplot = retrieve_args(kwargs);
+
+    if rlplot
+        #We are interested in the objectives plot only
+        num_subplot_y = BAN.SIZE;
+    end
+    
+    fig = figure(figtitle,figsize=figsize)
+    subplots_adjust(hspace=0.0)
+    PyPlot.title(title)
+    for i=1:num_subplot_x
+        # generate the subplots for the current magnitude of x from top to bottom
+        axs = [];
+        push!(axs, subplot(num_subplot_y, num_subplot_x, i, ylabel="\$α^{$(max_degree_y)}\$"));
+        for j = 2:num_subplot_y
+            push!(axs, subplot(num_subplot_y, num_subplot_x, i+(j-1)*num_subplot_x, sharex = axs[j-1], ylabel="\$α^{$(max_degree_y-j+1)}\$"));
+        end
+        xlabel("\$α^{$(min_degree_x+i-1)}\$");
+        
+        # supposing to plot a function, i.e., x increasing in value monotonically
+        idx = findfirst(z->z>i+min_degree_x-1, degx);
+        if idx == nothing 
+            idx = length(degx);
+        else
+            idx -= 1;
+        end
+
+        ref_deg_y = degy[start_idx];
+        intervals_y = [];  
+
+        for j=start_idx:idx #since they are consecutive indexes
+            degy[j] != ref_deg_y && (push!(intervals_y, (start_idx, j-1)); start_idx=j; ref_deg_y=degy[j]; true)
+        end
+        push!(intervals_y, (start_idx, idx));
+
+        if rlplot #if reinforcement learning reward plot flag is active
+            for j in intervals_y
+                #if first objective value is non-zero, simply plot each reward
+                if degy[j[1]]==max_degree_y
+                        if j[1]==j[2]
+                            axs[1].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                            axs[2].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num2 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                            axs[3].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num3 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                        else   
+                            axs[1].plot([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], c=:steelblue);
+                            axs[2].plot([z.num1 for z in x[j[1]:j[2]]], [z.num2 for z in y[j[1]:j[2]]], c=:steelblue);
+                            axs[3].plot([z.num1 for z in x[j[1]:j[2]]], [z.num3 for z in y[j[1]:j[2]]], c=:steelblue);
+                        end
+                #if first objective value is zero, plot zero(es) for the interval in all objectives prior the first degree of Ban's normal form
+                else
+                    for n=1:max_degree_y-degy[j[1]]
+                        if j[1]==j[2]
+                            axs[n].scatter([z.num1 for z in x[j[1]:j[2]]], 0, s=3,c = :steelblue);
+                        else   
+                            axs[n].plot([z.num1 for z in x[j[1]:j[2]]], zeros(j[2]-j[1]+1), c=:steelblue);
+                        end
+                    end
+                    n=BAN.SIZE-(max_degree_y-degy[j[1]])
+                    if n==1
+                        if j[1]==j[2]
+                            axs[3].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                        else   
+                            axs[3].plot([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], c=:steelblue);
+                        end
+                    elseif n==2 
+                        if j[1]==j[2]
+                            axs[2].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                            axs[3].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num2 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                        else   
+                            axs[2].plot([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], c=:steelblue);
+                            axs[3].plot([z.num1 for z in x[j[1]:j[2]]], [z.num2 for z in y[j[1]:j[2]]], c=:steelblue);
+                        end
+                    end
+                end
+            end
+        else
+            for j in intervals_y
+                if j[1]==j[2]
+                    axs[max_degree_y-degy[j[1]]+1].scatter([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], s=3,c = :steelblue);
+                else   
+                    axs[max_degree_y-degy[j[1]]+1].plot([z.num1 for z in x[j[1]:j[2]]], [z.num1 for z in y[j[1]:j[2]]], c=:steelblue);
                 end
             end
         end
